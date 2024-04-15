154a4ac4a0429a98714c6a4984fe6efe46563c4b
NarooSister <dongeun49@naver.com>
2024-04-15 13:00:45 +0900

    init

---
+++ src/main/java/com/example/homeGym/HomeGymApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeGymApplication.class, args);
+	}
+
+}
