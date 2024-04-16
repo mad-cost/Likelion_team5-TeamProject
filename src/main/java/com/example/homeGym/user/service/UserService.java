b9eaa0b83f2824cc2f69300b0bd38c1e61751387
mad-cost <mad_scientist1998@naver.com>
2024-04-16 17:57:24 +0900

    [feat]admin_first

--- src/main/java/com/example/homeGym/user/service/UserService.java
+++ src/main/java/com/example/homeGym/user/service/UserService.java
@@ -1,13 +1,31 @@
 package com.example.homeGym.user.service;
 
+import com.example.homeGym.user.dto.UserDto;
+import com.example.homeGym.user.entity.User;
 import com.example.homeGym.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+
+
+
+    public List<UserDto> findAllByOrderByName(){
+        List<UserDto> userDtos = new ArrayList<>();
+        for (User user : userRepository.findAllByOrderByName()){
+            userDtos.add(UserDto.fromEntity(user));
+        }
+        return userDtos;
+    }
+
 }
